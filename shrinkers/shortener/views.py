19145a64b08c500db6f506179edbff1c2964fec8
mjh9401 <mjh9401@naver.com>
2022-11-12 23:39:24 +0900

    get,post 데이터 전송 실습

--- shrinkers/shortener/views.py
+++ shrinkers/shortener/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,redirect
 from shortener.models import Users
+from django.views.decorators.csrf import csrf_exempt
+from django.http.response import JsonResponse
 
 # Create your views here.
 
@@ -16,5 +18,15 @@ def index(request):
         email = "Anonymous User!"
     return render(request,"base.html",{"welcome_msg" : f"Hello {email}"})
 
-def redirect_test(request):
-    return redirect("index")
+@csrf_exempt
+def get_user(request,user_id):
+    if request.method == 'GET':
+        abc = request.GET.get("abc")
+        xyz = request.GET.get("xyz")
+        user = Users.objects.filter(pk=user_id).first()
+        return render(request,"base.html",{"user":user, "params":[abc,xyz]})
+    elif request.method == "POST":
+        username = request.GET.get("username")
+        if username :
+            user = Users.objects.filter(pk=user_id).update(username=username)
+        return JsonResponse(status=201, data = dict(msg="You just reached with Post Method!"), safe=False)
